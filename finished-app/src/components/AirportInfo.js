cd0bb0583df13a994f811b8f14abc2a9ba18e4bf
Sara <sarayaoliu@gmail.com>
2021-03-08 05:45:25 -0500

    added form and adjusted layout

--- finished-app/src/components/AirportInfo.js
+++ finished-app/src/components/AirportInfo.js
@@ -4,7 +4,10 @@ import Places from './Places';
 
 function AirportInfo() { 
     const [places,setPlaces] = useState([])
-    const [query,setQuery] = useState("")
+    const [originplace,setOriginplace] = useState("")
+    const [destinationplace,setDestinationplace] = useState("")
+    const [outboundpartialdate,setOutboundpartialdate] = useState("")
+    const [inboundpartialdate,setInboundpartialdate] = useState("")
     const [showPlaces,setShowPlaces] = useState(false)
 
     function handleSubmit(e) {
@@ -15,27 +18,52 @@ function AirportInfo() {
                 headers: {
                     "x-rapidapi-key": `${process.env.REACT_APP_API_KEY}`, //need to remove <> and add "" around the key in env
                     "x-rapidapi-host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
-                    "useQueryString": true
+                    "useQueryString": false
                 }
             }
             // console.log(process.env.REACT_APP_API_KEY)
             // console.log(`${process.env.REACT_APP_API_KEY}`)
-            let response = await fetch("https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/autosuggest/v1.0/US/USD/en-US/?" + new URLSearchParams({query: query}), reqOptions)
+            const qString = {
+                "originplace": originplace,
+                "destinationplace": destinationplace,
+                "outboundpartialdate": outboundpartialdate,
+                "inboundpartialdate": inboundpartialdate
+            }
+            let response = await fetch("https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsedates/v1.0/US/USD/en-US/" + new URLSearchParams(qString), reqOptions)
+            // let response = await fetch("https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/US/USD/en-US/SFO-sky/JFK-sky/2019-01-01?inboundpartialdate=2019-09-01",
+            //     reqOptions) //doesn't work
+            // let response = await fetch("https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsedates/v1.0/US/USD/en-US/AMS-sky/LAX-sky/2021-03/2021-04", reqOptions) //works
+            // let response = await fetch("https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/US/USD/en-US/?" + new URLSearchParams({originplace: originplace})
+            //     + "/" + new URLSearchParams({destinationplace: destinationplace}), reqOptions) //doesn't work
+            // let response = await fetch("https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/US/USD/en-US/?" +  originplace + "/" + destinationplace, reqOptions)
+            console.log(response)
             response = await response.json()
+            console.log(response)
             // console.log(response) //{Places: Array(10)}
             setPlaces(response.Places)
             console.log(response.Places) //Warning: Each child in a list should have a unique "key" prop.
         }
         fetchMyAPI()
         setShowPlaces(true)
-        setQuery("")
+        setOriginplace("")
+        setDestinationplace("")
     }
 
     return(
         <div className="airportinfo">
            <form onSubmit={handleSubmit}>
-                <label htmlFor="queryInput">State or Country:</label>
-                <input id="queryInput" value={query} onChange={e => setQuery(e.target.value)} required/>
+                <label htmlFor="queryInput">From:</label>
+                <input id="queryInput" value={originplace} onChange={e => setOriginplace(e.target.value)} required/>
+                <label htmlFor="queryInput">To:</label>
+                <input id="queryInput" value={destinationplace} onChange={e => setDestinationplace(e.target.value)} required/>
+                <br />
+                <label htmlFor="queryInput">Depart:</label>
+                <input id="queryInput" value={outboundpartialdate} onChange={e => setOutboundpartialdate(e.target.value)} required/>
+                <label htmlFor="queryInput">Arrive:</label>
+                <input id="queryInput" value={inboundpartialdate} onChange={e => setInboundpartialdate(e.target.value)} required/>
+
+                {/* <label htmlFor="queryInput">State or Country:</label> //need to comment with braces */}
+                {/* <input id="queryInput" value={query} onChange={e => setQuery(e.target.value)} required/> */}
                 <button className="search">Submit</button>
            </form>
            { showPlaces ? <Places places={places}></Places> : <></>}
